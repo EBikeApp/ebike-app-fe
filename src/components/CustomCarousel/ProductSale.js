d1fac81190e5c210e9e9bfee004a9f9fd10ef0f4
MinhTriet0612 <ntriet0612@gmail.com>
2024-04-07 00:01:58 +0700

    added feature for homepage

---
+++ src/components/CustomCarousel/ProductSale.js
@@ -0,0 +1,103 @@
+import { db } from "@/config/firebase/firebase";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import Product from "../ProductComponent/Product";
+import Carousel from "react-slick";
+import "@/pages/styles";
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <button
+      className={className}
+      style={{
+        ...style,
+        width: "30px",
+      }}
+      onClick={() => onClick()}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <button
+      className={className}
+      style={{
+        ...style,
+        width: "30px",
+      }}
+      onClick={() => onClick()}
+    />
+  );
+}
+
+export default function ProductSale() {
+  const [listProducts, setListProducts] = useState([]);
+
+  useEffect(() => {
+    queryProductsFunc();
+  }, []);
+
+  const queryProductsFunc = async () => {
+    const q = query(
+      collection(db, "products"),
+      orderBy("discountPercentage", "desc"),
+    );
+    const querySnapshot = await getDocs(q);
+    const data = querySnapshot.docs.map((doc) => doc.data());
+    setListProducts(data.slice(0, 6));
+  };
+
+  const settings = {
+    // dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          // dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <div className="mt-14">
+      <h5 className="text-2xl font-semibold text-center">
+        SẢN PHẨM KHUYẾN MÃI
+      </h5>
+      <div className="w-[80%]  m-auto">
+        <Carousel {...settings}>
+          {listProducts.map((product, index) => (
+            <Product key={index} product={product} index={index} />
+          ))}
+        </Carousel>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,63 +1,93 @@
 "use client";
-import React from 'react';
+import React from "react";
 import styles from "@/pages/styles.js";
 import { makeStyles } from "@material-ui/core/styles";
-import Button from '@/components/CustomButtons/Button.js';
-import CategoryButton from '@/pages/CategoryButton';
+import Button from "@/components/CustomButtons/Button.js";
+import CategoryButton from "@/pages/CategoryButton";
 import SectionCarousel from "@/components/Carousel";
-import GridItem from '@/components/Grid/GridItem';
-import GridContainer from '@/components/Grid/GridContainer';
-import Link from 'next/link';
+import GridItem from "@/components/Grid/GridItem";
+import GridContainer from "@/components/Grid/GridContainer";
+import Link from "next/link";
+import OutStandingProducts from "@/components/CustomCarousel/OutStandingProducts";
+import ProductSale from "@/components/CustomCarousel/ProductSale";
 
 const useStyles = makeStyles(styles);
 
 export default function Home() {
-  const classes =  useStyles();
-  
+  const classes = useStyles();
+
   const introduceSession = (
-      <div className={classes.sessionIntroduction}>
-        <h5 className={classes.sessionTitle}>GIỚI THIỆU</h5>
-        <p>Chào mừng đến với Cửa hàng Xe Đạp - Xe Điện Thanh Tùng!</p>
-        <p>
-          Tại Thanh Tùng, chúng tôi mang đến cho bạn sự lựa chọn đa dạng và chất lượng cao về xe đạp và xe điện. Với một mục tiêu duy nhất là đáp ứng nhu cầu vận chuyển của cộng đồng một cách hiệu quả và thú vị, chúng tôi không ngừng cải tiến dịch vụ và sản phẩm của mình để mang lại trải nghiệm tốt nhất cho khách hàng.
-        </p>
-        <p>
-          Chúng tôi tự hào với sự đa dạng của bộ sưu tập xe đạp và xe điện của mình, mang
-          lại những giá trị về công nghệ tiên tiến, an toàn qua các phương tiện
-          như: xe đạp, xe đạp điện, xe máy điện được nghiên cứu và sản xuất bởi
-          các thương hiệu hàng đầu như: ASAMA, JVC,…
-        </p>
-        <div className={classes.sessionTitle}>
-          <Link href="/gioi-thieu">
-            <Button type="button" color="info">Xem thêm</Button>
-          </Link>
-        </div>
+    <div className={classes.sessionIntroduction}>
+      <h5 className={classes.sessionTitle}>GIỚI THIỆU</h5>
+      <p>Chào mừng đến với Cửa hàng Xe Đạp - Xe Điện Thanh Tùng!</p>
+      <p>
+        Tại Thanh Tùng, chúng tôi mang đến cho bạn sự lựa chọn đa dạng và chất
+        lượng cao về xe đạp và xe điện. Với một mục tiêu duy nhất là đáp ứng nhu
+        cầu vận chuyển của cộng đồng một cách hiệu quả và thú vị, chúng tôi
+        không ngừng cải tiến dịch vụ và sản phẩm của mình để mang lại trải
+        nghiệm tốt nhất cho khách hàng.
+      </p>
+      <p>
+        Chúng tôi tự hào với sự đa dạng của bộ sưu tập xe đạp và xe điện của
+        mình, mang lại những giá trị về công nghệ tiên tiến, an toàn qua các
+        phương tiện như: xe đạp, xe đạp điện, xe máy điện được nghiên cứu và sản
+        xuất bởi các thương hiệu hàng đầu như: ASAMA, JVC,…
+      </p>
+      <div className={classes.sessionTitle}>
+        <Link href="/gioi-thieu">
+          <Button type="button" color="info">
+            Xem thêm
+          </Button>
+        </Link>
       </div>
+    </div>
   );
   const categorySession = (
-      <div className={classes.sessionCategory}>
-        <h5 className={classes.sessionTitle}>DANH MỤC SẢN PHẨM</h5>
-        <GridContainer>
-          <GridItem xs={6} sm={6} md={3}>
-            <CategoryButton url="/san-pham/xe-dap" title="Xe đạp" category="Xe đạp" imagePath="/bicycle.png" />
-          </GridItem>
-          <GridItem xs={6} sm={6} md={3}>
-            <CategoryButton url="/san-pham/xe-dap-dien" title="Xe đạp điện" category="Xe đạp điện" imagePath="/electric-bike.png" />
-          </GridItem>
-          <GridItem xs={6} sm={6} md={3}>
-            <CategoryButton url="/san-pham/xe-may-dien" title="Xe đạp" category="Xe máy điện" imagePath="/electric-motor.png" />
-          </GridItem>
-          <GridItem xs={6} sm={6} md={3}>
-            <CategoryButton url="/san-pham/phu-kien" title="Phụ kiện" category="Phụ kiện" imagePath="/spare-parts.png" />
-          </GridItem>
-        </GridContainer>
-      </div>
+    <div className={classes.sessionCategory}>
+      <h5 className={classes.sessionTitle}>DANH MỤC SẢN PHẨM</h5>
+      <GridContainer>
+        <GridItem xs={6} sm={6} md={3}>
+          <CategoryButton
+            url="/san-pham/xe-dap"
+            title="Xe đạp"
+            category="Xe đạp"
+            imagePath="/bicycle.png"
+          />
+        </GridItem>
+        <GridItem xs={6} sm={6} md={3}>
+          <CategoryButton
+            url="/san-pham/xe-dap-dien"
+            title="Xe đạp điện"
+            category="Xe đạp điện"
+            imagePath="/electric-bike.png"
+          />
+        </GridItem>
+        <GridItem xs={6} sm={6} md={3}>
+          <CategoryButton
+            url="/san-pham/xe-may-dien"
+            title="Xe đạp"
+            category="Xe máy điện"
+            imagePath="/electric-motor.png"
+          />
+        </GridItem>
+        <GridItem xs={6} sm={6} md={3}>
+          <CategoryButton
+            url="/san-pham/phu-kien"
+            title="Phụ kiện"
+            category="Phụ kiện"
+            imagePath="/spare-parts.png"
+          />
+        </GridItem>
+      </GridContainer>
+    </div>
   );
   return (
     <div className={classes.root}>
-        <SectionCarousel />
-        {introduceSession}
-        {categorySession}
+      <SectionCarousel />
+      {introduceSession}
+      {categorySession}
+      <OutStandingProducts />
+      <ProductSale />
     </div>
   );
 }
