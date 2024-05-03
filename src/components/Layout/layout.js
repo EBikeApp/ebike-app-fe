9e559d47210032fd6375bf60371e33d865d7cb6d
AlexNg <ntriet0612@gmail.com>
2024-05-03 10:52:22 +0700

    modified a little bit about layout

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -6,7 +6,9 @@ export default function Layout({ children }) {
   return (
     <div className="w-full">
       <Header />
-      {children}
+      <div className="mt-20 md:mt-0">
+        {children}
+      </div>
       <Footer />
       <div className="fixed bottom-9 right-2 z-20">
         <ButtonForContactUs />
