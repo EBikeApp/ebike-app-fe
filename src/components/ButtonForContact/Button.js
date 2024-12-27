aa55d73374508d55917d33628cc8c8a248fa04de
alexNg <ntriet0612@gmail.com>
2024-12-28 00:16:55 +0700

    fix button

--- src/components/ButtonForContact/Button.js
+++ src/components/ButtonForContact/Button.js
@@ -1,14 +1,15 @@
 import Link from "next/link";
 
+
 export default function ButtonForContactUs() {
-  const tiktokLogo = "https://hanayuki.com.vn/images/tiktok.png";
+  const facebook = require("../../assest/images/facebook-svgrepo-com.svg");
 
   return (
     <div className="flex flex-col items-center gap-2">
       <Link target="_blank" href="https://www.facebook.com/profile.php?id=61550772461381&ref=embed_page">
         <div className="bg-sky-500 inline-block rounded-full w-11 h-11 p-2">
           <img
-            src="https://hanayuki.com.vn/images/icon-facebook.png"
+            src="https://www.svgrepo.com/show/3885/facebook.svg"
             alt="facebook"
             className="w-full"
           />
@@ -17,7 +18,7 @@ export default function ButtonForContactUs() {
       <Link target="_blank" href="https://www.facebook.com/messages/t/131507896704269">
         <div className="bg-sky-500 inline-block rounded-full w-11 h-11 p-2">
           <img
-            src="https://hanayuki.com.vn/images/ic-messenger.png"
+            src="https://www.svgrepo.com/show/313913/facebook-messenger.svg"
             alt="messenger"
             className="w-full"
           />
@@ -26,7 +27,7 @@ export default function ButtonForContactUs() {
       <Link target="_blank" href="https://zalo.me/0974294781">
         <div className="bg-sky-500 inline-block rounded-full w-11 h-11 p-2">
           <img
-            src="https://hanayuki.com.vn/images/zalo.png"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Icon_of_Zalo.svg/1200px-Icon_of_Zalo.svg.png"
             alt="zalo"
             className="w-full"
           />
@@ -35,7 +36,7 @@ export default function ButtonForContactUs() {
       <Link target="_blank" href="https://www.tiktok.com/@xedap_xedien_thanhtung?_t=8m0hDEdT65y&_r=1">
         <div className="bg-sky-500 inline-block rounded-full w-11 h-11 p-2">
           <img
-            src="https://hanayuki.com.vn/images/tiktok.png"
+            src="https://www.svgrepo.com/show/364932/tiktok-logo-fill.svg"
             alt="tiktok"
             className="w-full"
           />
@@ -45,7 +46,7 @@ export default function ButtonForContactUs() {
         <a target="_blank" href="tel:0974294781">
           <div className="bg-sky-500 inline-block rounded-full w-11 h-11 p-2">
             <img
-              src="https://hanayuki.com.vn/images/icon-call.png"
+              src="https://cdn1.iconfinder.com/data/icons/office-and-business-14/48/99-512.png"
               alt="Phone"
               className="w-full"
             />
